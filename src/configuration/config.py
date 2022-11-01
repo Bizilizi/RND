62b603935cad3465e66cd44790d6b9269acfdde1
ewriji <ewriji@gmail.com>
2022-11-01 15:22:46 +0100

    Init commit

---
+++ src/configuration/config.py
@@ -0,0 +1,37 @@
+from configparser import ConfigParser
+
+from pydantic import BaseModel
+
+
+class TrainConfig(BaseModel):
+    # Training
+    gpus: str
+    batch_size: int
+    resume_ckpt_path: str
+    max_epochs: int
+    validate_every_n: int
+    num_workers: int
+    accumulate_grad_batches: int
+    learning_rate: float
+
+    # Logging
+    logger_type: str
+    logging_path: str
+
+    @staticmethod
+    def construct_typed_config(ini_config: ConfigParser) -> "TrainConfig":
+        """
+        Creates typed version of ini configuration file
+
+        :param ini_config: ConfigParser instance
+        :return: Instance of TrainConfig
+        """
+
+        config = TrainConfig(
+            **ini_config["training"],
+            **ini_config["dataset"],
+            **ini_config["logging"],
+            **ini_config["model"],
+        )
+
+        return config
