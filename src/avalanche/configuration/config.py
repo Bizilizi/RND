c0d6b6495db809b3920d9a7e4d3a28e35c18c883
ewriji <ewriji@gmail.com>
2023-02-03 14:06:54 +0000

    feat: VAE - better config

--- src/avalanche/configuration/config.py
+++ src/avalanche/configuration/config.py
@@ -1,6 +1,21 @@
 from configparser import ConfigParser
 
 from pydantic import BaseModel
+import typing as t
+from pydantic.validators import str_validator
+
+
+def empty_to_none(v: str) -> t.Optional[str]:
+    if v == "":
+        return None
+    return v
+
+
+class EmptyStrToNone(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield str_validator
+        yield empty_to_none
 
 
 class BaseTrainConfig(BaseModel):
@@ -12,7 +27,7 @@ class BaseTrainConfig(BaseModel):
     min_epochs: int
     validate_every_n: int
     num_workers: int
-    accumulate_grad_batches: int
+    accumulate_grad_batches: t.Union[None, int, EmptyStrToNone]
     learning_rate: float
 
     # Logging
--- src/avalanche/strategies/naive_pl.py
+++ src/avalanche/strategies/naive_pl.py
@@ -1,4 +1,5 @@
 import typing as t
+from copy import deepcopy
 
 from pytorch_lightning import Callback, Trainer
 
