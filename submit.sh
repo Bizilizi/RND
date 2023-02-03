827e102954bef619fe9fe981397b09497baa68ec
ewriji <ewriji@gmail.com>
2023-02-03 16:30:40 +0000

    feat: VAE - better config

--- submit.sh
+++ submit.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=72:00:00                     # Time limit hrs:min:sec
 #SBATCH --nodes=1                           # Node count
 #SBATCH --ntasks=1                          # Total number of tasks across all nodes
-#SBATCH --cpus-per-task=8                   # Number of CPU cores per task
-#SBATCH --mem=12gb                          # Job memory request
+#SBATCH --cpus-per-task=2                   # Number of CPU cores per task
+#SBATCH --mem=2gb                          # Job memory request
 #SBATCH --partition=gpu                     # Partition (compute (default) / gpu)
 #SBATCH --gres=gpu:1                        # Requesting 1 GPUs
 #SBATCH --constraint=p40,m40
