0c20dfb99b3df05a4d6603bcfaff929e88e93417
ewriji <ewriji@gmail.com>
2023-02-24 14:39:00 +0100

    fix: Noise - fix noise sampling issue

--- train.py
+++ train.py
@@ -319,7 +319,6 @@ def main(args):
 
 
 if __name__ == "__main__":
-    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
     parser = argparse.ArgumentParser(description="Model trainer")
     parser = add_arguments(parser)
