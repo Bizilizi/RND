64bfce275838aca36c463a3aa03df4e40c8f87d5
ewriji <ewriji@gmail.com>
2023-02-26 16:05:35 +0100

    fix: Sampling - Fix bug related to sampling

--- src/vae_ft/model/vae.py
+++ src/vae_ft/model/vae.py
@@ -64,13 +64,11 @@ class MLPVae(CLModel):
     ) -> t.Tuple[torch.Tensor, torch.Tensor]:
         x_pred, x_input, log_sigma, mu = x
 
-        if (
-            not torch.isfinite(x_pred.flatten()).all()
-            or not torch.isfinite(x_input.flatten()).all()
-        ):
+        if not torch.isfinite(x_pred.flatten()).all():
             for name, params in self.named_parameters():
                 assert torch.isfinite(params).all(), name
 
+            print(x_pred.flatten())
             assert torch.isfinite(x_pred.flatten()).all(), "input"
             assert torch.isfinite(x_input.flatten()).all(), "target"
 
