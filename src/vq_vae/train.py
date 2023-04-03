38aa5a3b3c5fc95ccd39af50b00a8448a6e7c5c2
ewriji <ewriji@gmail.com>
2023-04-03 02:16:22 +0100

    feat: VQ-VAE - fix logging errors and templates

--- src/vq_vae/train.py
+++ src/vq_vae/train.py
@@ -118,7 +118,7 @@ def main(args):
         n_experiences=1,
         return_task_id=True,
         shuffle=True,
-        dataset_root="/tmp/dzverev_data",
+        # dataset_root="/tmp/dzverev_data",
     )
 
     device = get_device(config)
