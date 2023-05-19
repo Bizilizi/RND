605a072cf3f97ab33df3858fc232e6a4717ea7ff
ewriji <ewriji@gmail.com>
2023-05-19 17:13:10 +0100

    feat: Transformer-VQ-VAE - notebooks

--- src/transformer_vq_vae/train.py
+++ src/transformer_vq_vae/train.py
@@ -173,7 +173,7 @@ def main(args):
     config.bootstrapped_dataset_path += f"/{run_id}/best_model"
 
     benchmark = SplitCIFAR10(
-        n_experiences=1,
+        n_experiences=5,
         return_task_id=True,
         shuffle=True,
         dataset_root=config.dataset_path,
