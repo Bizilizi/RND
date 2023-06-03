6fa474d77886697e6611f685715181a49a76645e
ewriji <ewriji@gmail.com>
2023-06-03 13:30:31 +0200

    feat: Transformer-VQ-VAE - cycle loss backbone

--- submit.sh
+++ submit.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name=sbatch_job               # Job name
 #SBATCH --mail-type=BEGIN,END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=dzverev@robots.ox.ac.uk # Where to send mail
-#SBATCH --time=72:00:00                     # Time limit hrs:min:sec
+#SBATCH --time=92:00:00                     # Time limit hrs:min:sec
 #SBATCH --nodes=1                           # Node count
 #SBATCH --ntasks=1                          # Total number of tasks across all nodes
-#SBATCH --cpus-per-task=4                   # Number of CPU cores per task
-#SBATCH --mem=24gb                          # Job memory request
-#SBATCH --partition=gpu                     # Partition (compute (default) / gpu)
+#SBATCH --cpus-per-task=8                   # Number of CPU cores per task
+#SBATCH --mem=48gb                          # Job memory request
+#SBATCH --partition=gpu           # Partition (compute (default) / gpu)
 #SBATCH --gres=gpu:1                        # Requesting 1 GPUs
-#SBATCH --constraint=p40
+#SBATCH --constraint=a6000
 # -------------------------------
 echo $TIMM_FUSED_ATTN
 nvidia-smi
