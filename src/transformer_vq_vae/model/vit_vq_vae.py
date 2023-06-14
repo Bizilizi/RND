e3225e9418bd73777cee7afe70097ad07ae5196c
ewriji <ewriji@gmail.com>
2023-06-14 23:23:05 +0100

    feat: Transformer-VQ-VAE - top_k neighbours

--- src/transformer_vq_vae/model/vit_vq_vae.py
+++ src/transformer_vq_vae/model/vit_vq_vae.py
@@ -251,24 +251,24 @@ class VitVQVae(CLModel):
         )
 
         if self._quantize_features:
-            # with torch.autocast(self._accelerator, dtype=torch.float32):
-            (
-                vq_loss,
-                masked_features,
-                feature_perplexity,
-                class_perplexity,
-                *_,
-            ) = self.feature_quantization(masked_features)
-            """
-            masked_features shape - T x B x top_k x emb_dim
-            """
-            masked_features = masked_features.mean(2)
-            """
-            masked_features shape - T x B x emb_dim
-            """
-            (*_, latent_distances) = self.feature_quantization(
-                full_features, return_distances=True
-            )
+            with torch.autocast(self._accelerator, dtype=torch.float32):
+                (
+                    vq_loss,
+                    masked_features,
+                    feature_perplexity,
+                    class_perplexity,
+                    *_,
+                ) = self.feature_quantization(masked_features)
+                """
+                masked_features shape - T x B x top_k x emb_dim
+                """
+                masked_features = masked_features.mean(2)
+                """
+                masked_features shape - T x B x emb_dim
+                """
+                (*_, latent_distances) = self.feature_quantization(
+                    full_features, return_distances=True
+                )
 
         x_recon, mask = self.decoder(masked_features, backward_indexes)
 
