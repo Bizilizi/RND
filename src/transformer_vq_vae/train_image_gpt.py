a4d99cd5b5bf2b7e578d5e3335abdc7e0267ea5a
dpzverev <dpzverev@gid.ru>
2024-01-22 00:24:48 +0100

    feat: image gpt - remove separate codebook

--- src/transformer_vq_vae/train_image_gpt.py
+++ src/transformer_vq_vae/train_image_gpt.py
@@ -224,7 +224,7 @@ def train_igpt(
     )
 
     if strategy.experience_step < 2:
-        epoch_num = 1
+        epoch_num = 10
     elif strategy.experience_step < 3:
         epoch_num = 10
     else:
