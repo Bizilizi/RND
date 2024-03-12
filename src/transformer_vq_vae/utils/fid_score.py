cd0de6f06c5d8bf768a20701fc704a265a4f602a
dpzverev <dpzverev@gid.ru>
2024-03-12 19:16:05 +0100

    feat: qmae - fid_score

--- src/transformer_vq_vae/utils/fid_score.py
+++ src/transformer_vq_vae/utils/fid_score.py
@@ -259,22 +259,6 @@ def calculate_activation_statistics(
     return mu, sigma
 
 
-def compute_statistics_of_path(path, model, batch_size, dims, device, num_workers=1):
-    if path.endswith(".npz"):
-        with np.load(path) as f:
-            m, s = f["mu"][:], f["sigma"][:]
-    else:
-        path = pathlib.Path(path)
-        files = sorted(
-            [file for ext in IMAGE_EXTENSIONS for file in path.glob("*.{}".format(ext))]
-        )
-        m, s = calculate_activation_statistics(
-            files, model, batch_size, dims, device, num_workers
-        )
-
-    return m, s
-
-
 def calculate_fid_given_datasets(
     dataset_1, dataset_2, batch_size, device, dims, num_workers=1
 ):
