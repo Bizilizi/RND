39b53b4f155512ce5b42803a18278c5fb52677e2
dpzverev <dpzverev@gid.ru>
2024-03-20 23:43:31 +0100

    feat: qmae - improve fid score script

--- src/qmae_latent_extension/train_image_gpt.py
+++ src/qmae_latent_extension/train_image_gpt.py
@@ -281,8 +281,6 @@ def train_igpt(
                     masked_input_ids[..., 1:].reshape(-1),
                 )
                 grad_scaler.scale(loss).backward()
-            # REMOVE
-            break
             if step % config.igpt_accumulate_grad_batches == 0:
                 grad_scaler.step(optimizer)
                 grad_scaler.update()
@@ -297,8 +295,6 @@ def train_igpt(
                     },
                     step=i,
                 )
-        # REMOVE
-        break
         # Generate sampled images at the end of the epoch
         if local_rank == 0:
             if is_distributed:
