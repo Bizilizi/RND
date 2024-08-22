f70c22fbf55a397808f2ba76dbd230fd3bca60af
Bizilizi <kabara.bizilizi@gmail.com>
2024-08-22 22:50:59 +0200

    feat: gen-collaps - add restore_from for gan

--- src/gen_collaps/stylegan2/main.py
+++ src/gen_collaps/stylegan2/main.py
@@ -627,9 +627,22 @@ def main(restore_from: str = None):
         )
 
 
+def resample(restore_from):
+    assert restore_from, "argument restore_from cannot be None or empty"
+
+    # Create configurations object
+    configs = Configs()
+    _, synthetic_dataset_path = restore_from_previous_step(configs, restore_from)
+    sample_synthetic_dataset(configs, synthetic_dataset_path)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="stylegan trainer")
     parser.add_argument("--restore_from", type=str, help="experiment path", default=None)
+    parser.add_argument("--command", type=str, help="command", default="train")
     args = parser.parse_args()
 
-    main(args.restore_from)
+    if args.command == "resample":
+        resample(args.restore_from)
+    else:
+        main(args.restore_from)
